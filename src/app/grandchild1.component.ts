c9c9f324883e27b8b024dfc9cb5062b5c1fe6adb
Sam Lin <saml@millerslab.com>
2024-03-29 11:43:10 -0500

    use counter object

--- src/app/grandchild1.component.ts
+++ src/app/grandchild1.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-grandchild1',
   template: `
     <h3>Grandchild 1 Component</h3>
-    <p>Counter: {{ counter }}</p>
+    <p>Counter: {{ counter.count }}</p>
     <button (click)="incCounter()">Inc Counter</button>
     {{ logCD() }}
   `,
@@ -23,9 +23,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class Grandchild1Component {
-  counter = 0;
+  counter = { count: 0 };
   incCounter() {
-    this.counter++;
+    this.counter.count++;
   }
   logCD() {
     console.log('Grandchild 1 Component - Change Detection');
